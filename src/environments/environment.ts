79824d923f634ee4d9506e0b8efda0002b1d9766
rastafasha <mercadocreativo@gmail.com>
2024-02-29 20:50:57 -0400

    produccion  primer trial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,10 +10,16 @@ export const environment = {
   // url_frontend: 'http://localhost:4200/',
   // url_media: 'http://127.0.0.1:8000/storage/',
   //remoto
-  url_backend: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/',
-  url_servicios: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/public/api',
-  url_frontend: 'https://paciente.malcolmcordova.com/',
-  url_media: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/storage/app/public/',
+  // url_backend: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/',
+  // url_servicios: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/public/api',
+  // url_frontend: 'https://paciente.malcolmcordova.com/',
+  // url_media: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/storage/app/public/',
+
+  //remoto cliente trial
+  url_backend: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/',
+  url_servicios: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/public/api',
+  url_frontend: 'https://pacsdleondeccs.malcolmcordova.com/',
+  url_media: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/storage/app/public/',
 };
 
 /*
