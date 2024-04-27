f261c74512d161acf28339c940644887000f7092
rastafasha <mercadocreativo@gmail.com>
2024-04-27 16:58:30 -0400

    environment solumedica

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,16 +16,22 @@ export const environment = {
   // url_media: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/storage/app/public/',
 
   //remoto cliente trial
-  url_backend: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/',
-  url_servicios: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/public/api',
-  url_frontend: 'https://pacsdleondeccs.malcolmcordova.com/',
-  url_media: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/storage/app/public/',
+  // url_backend: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/',
+  // url_servicios: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/public/api',
+  // url_frontend: 'https://pacsdleondeccs.malcolmcordova.com/',
+  // url_media: 'https://csdleondeccs.malcolmcordova.com/backend-api-citas/storage/app/public/',
 
   //remoto demo  dominio
   // url_backend: 'https://clinica.health-connect.me/backend-api-citas/',
   // url_servicios: 'https://clinica.health-connect.me/backend-api-citas/public/api',
   // url_frontend: 'https://paciente.health-connect.me/',
   // url_media: 'https://clinica.health-connect.me/backend-api-citas/storage/app/public/',
+
+  //remoto solucionamedica
+  url_backend: 'https://solucionamedica.health-connect.me/backend-api-citas/',
+  url_servicios: 'https://solucionamedica.health-connect.me/backend-api-citas/public/api',
+  url_frontend: 'https://pacsolucionamedica.health-connect.me/',
+  url_media: 'https://solucionamedica.health-connect.me/backend-api-citas/storage/app/public/',
 };
 
 /*
