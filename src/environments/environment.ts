9049b7e3891100054ec85a67c4c5e88a1182be0b
rastafasha <mercadocreativo@gmail.com>
2024-06-30 14:35:22 -0400

    arreglos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,30 +9,34 @@ export const environment = {
   // url_servicios: 'http://127.0.0.1:8000/api',
   // url_frontend: 'http://localhost:4200/',
   // url_media: 'http://127.0.0.1:8000/storage/',
-  //remoto
-  // url_backend: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/',
-  // url_servicios: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/public/api',
-  // url_frontend: 'https://paciente.malcolmcordova.com/',
-  // url_media: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/storage/app/public/',
-
-  //remoto cliente Clin Santiago de leon
-
-  url_backend: 'https://csdleondeccs.health-connect.me/backend-api-citas/',
-    url_servicios: 'https://csdleondeccs.health-connect.me/backend-api-citas/public/api',
-    url_frontend: 'https://pacsdleondeccs.health-connect.me/',
-    url_media: 'https://csdleondeccs.health-connect.me/backend-api-citas/storage/app/public/',
-
+  
   //remoto demo  dominio
   // url_backend: 'https://clinica.health-connect.me/backend-api-citas/',
   // url_servicios: 'https://clinica.health-connect.me/backend-api-citas/public/api',
   // url_frontend: 'https://paciente.health-connect.me/',
   // url_media: 'https://clinica.health-connect.me/backend-api-citas/storage/app/public/',
+  
 
-  //remoto solucionamedica
-  // url_backend: 'https://solucionamedica.health-connect.me/backend-api-citas/',
-  // url_servicios: 'https://solucionamedica.health-connect.me/backend-api-citas/public/api',
-  // url_frontend: 'https://pacsolucionamedica.health-connect.me/',
-  // url_media: 'https://solucionamedica.health-connect.me/backend-api-citas/storage/app/public/',
+  //remoto cliente Clin Santiago de leon
+
+    url_backend: 'https://csdleondeccs.health-connect.me/backend-api-citas/',
+    url_servicios: 'https://csdleondeccs.health-connect.me/backend-api-citas/public/api',
+    url_frontend: 'https://pacsdleondeccs.health-connect.me/',
+    url_media: 'https://csdleondeccs.health-connect.me/backend-api-citas/storage/app/public/',
+
+  
+
+  //remoto SOLUMEDICA
+  // url_backend: 'https://solumedica.health-connect.me/backend-api-citas/',
+  // url_servicios: 'https://solumedica.health-connect.me/backend-api-citas/public/api',
+  // url_frontend: 'https://psolumedica.health-connect.me/',
+  // url_media: 'https://solumedica.health-connect.me/backend-api-citas/storage/app/public/',
+
+  //remoto
+  // url_backend: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/',
+  // url_servicios: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/public/api',
+  // url_frontend: 'https://paciente.malcolmcordova.com/',
+  // url_media: 'https://citasmedicasv1.malcolmcordova.com/backend-api-citas/storage/app/public/',
 };
 
 /*
