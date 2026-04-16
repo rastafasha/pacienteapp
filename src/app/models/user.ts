e16f784f72f3fceb8af294e1efc0a5b5f3d2c627
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-04-16 12:17:11 -0400

    fixed env en modelos

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,4 @@
 
-import { environment } from "src/environments/environment";
-// const base_url = environment.apiUrlMedia;
 export class User {
 
     id!: number;
@@ -17,27 +15,5 @@ export class User {
     image: string = "";
     role?: 'SUPERADMIN' | 'ADMIN' | 'MEMBER' | 'GUEST';
     
-
-
-
-    // public get isActive():boolean{
-    //     return (this.is_active === 1 ? true: false);
-    // }
-
-
-    // get imagenUrl(){
-
-    //   if(!this.image){
-    //     return `${base_url}users/no-image.jpg`;
-    //   } else if(this.image.includes('https')){
-    //     return this.image;
-    //   } else if(this.image){
-    //     return `${base_url}users/${this.image}`;
-    //   }else {
-    //     return `${base_url}/no-image.jpg`;
-    //     // return `./assets/img/no-image.jpg`;
-    //   }
-
-    // }
 
 }
